47141268f8b1fd001a0f8dba203a580325a1f861
rodpd <rodrigo.pad@outlook.com.br>
2022-08-21 21:40:11 -0300

    style changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { useState } from 'react'
 
+
+
 function FilterableItemList(props) {
 
 	const [category, setCategory] = useState('All');
@@ -32,19 +34,24 @@ function Filter(props) {
 	}
 
 	return (
-	<select name="" id="" className="p-2 mb-4" onChange={handleChange}>
-		<option value="All">All</option>
-		<option value="Aerosol">Aerosol</option>
-		<option value="Alcohol en Aerosol">Alcohol en Aerosol</option>
-		<option value="Alcohol en Gel">Alcohol en Gel</option>
-		<option value="Alcohol en Spray">Alcohol en Spray</option>
-		<option value="Barra">Barra</option>
-		<option value="Jabón Barra">Jabón Barra</option>
-		<option value="Jabon Líquido">Jabon Líquido</option>
-		<option value="Talco">Talco</option>
-	</select>
+	<div className="flex justify-end mb-4">
+		<select name="" id="" 
+		className="p-2 mb-4 rounded-lg shadow-lg outline-none text-slate-800" 
+		onChange={handleChange}>
+			<option value="All">All</option>
+			<option value="Aerosol">Aerosol</option>
+			<option value="Alcohol en Aerosol">Alcohol en Aerosol</option>
+			<option value="Alcohol en Gel">Alcohol en Gel</option>
+			<option value="Alcohol en Spray">Alcohol en Spray</option>
+			<option value="Barra">Barra</option>
+			<option value="Jabón Barra">Jabón Barra</option>
+			<option value="Jabón Líquido">Jabón Líquido</option>
+			<option value="Talco">Talco</option>
+		</select>
+	</div>
 	)
 }
+
 
 function ItemsList(props) {
 
@@ -58,22 +65,23 @@ function ItemsList(props) {
 	);
 
 	return (
-		<div className='grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4'>
+		<div className='grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
 			{itemList}
 		</div>
 	)
 }
 
+
 function Item(props) {
 
 	return (
-		<div className="p-4 duration-300 bg-white rounded-lg shadow-lg hover:scale-105">
+		<div className="p-4 duration-300 ease-in-out bg-white rounded-lg shadow-lg cursor-default hover:scale-105 group">
 			<img
 			src={props.imgSrc}
 			alt={props.imgAlt}
 			className="h-[200px] mx-auto" />
 			<p className='pt-1 text-sm text-slate-500'>{props.category}</p>
-			<p className="pt-1 font-bold text-slate-800">{props.name}</p>
+			<p className="pt-1 font-bold text-slate-800 group-hover:text-blue-700">{props.name}</p>
 			<p className='pt-1 text-sm text-slate-800'>{props.description}</p>
 		</div>
 	);
@@ -86,7 +94,7 @@ function App() {
 	const data = require('./data/productsCategory.json').data.nodes;
 
 	return (
-    <main className="mx-[10%] sm:mx-[15%] my-[5%]">
+    <main className="mx-[10%] my-[5%]">
 		<FilterableItemList data={data}/>
     </main>
   );
